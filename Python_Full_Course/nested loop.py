5322bf06aa5ab6d7871d3985ae3beeb46c72b98b
Julio Tavares <j.tavares1840@gmail.com>
2024-10-26 11:38:48 -0300

    Update nested loop.py

--- Python_Full_Course/nested loop.py
+++ Python_Full_Course/nested loop.py
@@ -1,6 +1,5 @@
 # nested loop = The "inner loop" will finish all of it's iterations before
 #               finishing one iteration of the "outer loop"
-import time
 
 rows = int(input("How many rows?: "))
 columns = int(input("How many columns?: "))
