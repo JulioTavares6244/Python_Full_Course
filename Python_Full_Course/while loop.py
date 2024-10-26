8334c8f9def52fb59685cd25471632afe3d006d9
Julio Tavares <j.tavares1840@gmail.com>
2024-10-26 11:08:14 -0300

    Update while loop.py

--- Python_Full_Course/while loop.py
+++ Python_Full_Course/while loop.py
@@ -1,16 +1,16 @@
 # while loop = a statement that will execute it's block of code,
 #              as long as it's condition remains true
 
-# while 1==1:
-#     print("Help! I'm stuck in a loop!")
+#while 1==1:
+    #print("Help! I'm stuck in a loop!")
 
-# name = ""
+#name = ""
 name = None
 
-# while len(name) == 0:
-#     name = input("Enter your name: ")
+#while len(name) == 0:
+    #name = input("Enter your name: ")
 
-# print("Hello "+name)
+#print("Hello "+name)
 
 while not name:
     name = input("Enter your name: ")
